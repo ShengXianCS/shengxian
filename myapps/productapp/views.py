193b643e417fa78bc7bc37a8b03c285c38e1e019
yang94913 <38401249+yang94913@users.noreply.github.com>
2018-07-18 18:01:42 +0800

    '二'

--- myapps/productapp/views.py
+++ myapps/productapp/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(req):
-    return render(req,'home.html')
+    return render(req, 'home.html')
+
+def prodshow(req):
+    return render(req, 'productshow.html')
+
