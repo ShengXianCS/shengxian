28f40d52be7b0a2a8f57d81b4f747d298d45120d
edwin <edwin@qq.com>
2018-07-18 13:24:26 +0800

    添加内容 orderapp-views/urls cart.html

--- ZLShengXian/urls.py
+++ ZLShengXian/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
+from myapps import orderapp
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',TemplateView.as_view(template_name='home.html')),
+    url(r'admin/', admin.site.urls),
+    url(r'order/',include('orderapp.urls'))
 ]
--- myapps/orderapp/views.py
+++ myapps/orderapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def myCart(request):
+    return render(request,'cart.html')
