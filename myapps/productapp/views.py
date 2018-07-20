9eb2d7c8a6b862695f92c465eda864f2f8bd54f4
yang94913 <38401249+yang94913@users.noreply.github.com>
2018-07-20 22:20:09 +0800

    '第二页修改中。。。'

--- myapps/productapp/views.py
+++ myapps/productapp/views.py
@@ -6,69 +6,45 @@ from myapps.productapp.models import ProdTyp, Products
 
 
 def home(req):
-    return render(req, 'home.html', {'prodTyps':ProdTyp.objects.all()})
+    return render(req, 'home.html',
+                  {'prodTyps':ProdTyp.objects.all()})
 
-# , 'productlist': Products.objects.filter(prodTyp__typeid=id)
 
-def prodshow(req,sortid=0,categoryid=0,chilid=0):
-    productsList = None
-
-    sortColumn = 'salenums'
-    if sortid == 1:
-        sortColumn = '-price'
-    elif sortid == 2:
-        sortColumn ='price'
-    childTypes = []
-
-    if categoryid:
-        cTypes = ProdTyp.objects.filter(typeid = categoryid).last().productname
-        cTypes = cTypes.split('')
-        for ctype in cTypes:
-            ctype = ctype.splist()
-            childTypes.append({'name':ctype[0],'id':ctype[1]})
-
-        if chilid:
-            productsList = Products.objects.filter(categoryid=categoryid,chilid=chilid).order_by(sortColumn)
-        else:
-            productsList = Products.objects.filter(categoryid=categoryid).order_by(sortColumn)
-    else:
-        productsList = Products.objects.all().order_by(sortColumn)[0:10]
-    return render(req,'productshow.html',
-                  {'productTypes':ProdTyp.objects.order_by('typeid'),
-                   'productsList':productsList,
-                   'categoryid':str(categoryid),
-                   'childTypes':childTypes,
-                   'chilid':str(chilid),
-                   'sortid':sortid})
-def prodshow1(req,id,sortid=0,categoryid=0,chilid=0):
+def prodshow1(req, sortid=0, categoryid=0, chilid=0):
     productslist = None
 
-    sortColumn = 'salenums'
+    sortColumn = 'productid'
     if sortid == 1:
         sortColumn = '-price'
     elif sortid == 2:
         sortColumn = 'price'
-        childTypes = []
+    elif sortid == 3:
+        sortColumn = 'salenums'
+    childTypes = []
 
-        if categoryid:
-            cTypes = ProdTyp.objects.filter(typeid=categoryid).last().productname
-            cTypes = cTypes.split('')
-            for ctype in cTypes:
-                ctype = ctype.splist()
-                childTypes.append({'name': ctype[0], 'id': ctype[1]})
+    if categoryid:
 
-            if chilid:
-                productsList = Products.objects.filter(categoryid=categoryid, chilid=chilid).order_by(sortColumn)
-            else:
-                productsList = Products.objects.filter(categoryid=categoryid).order_by(sortColumn)
+        productlist = Products.objects.filter(typeid=categoryid).all().order_by(sortColumn)
+
+        cTypes = ProdTyp.objects.filter(typeid=categoryid).last().productname
+        cTypes = cTypes.split('')
+        for ctype in cTypes:
+            ctype = ctype.splist()
+            childTypes.append({'name': ctype[0], 'id': ctype[1]})
+
+        if chilid:
+            productslist = Products.objects.filter(categoryid=categoryid, chilid=chilid).order_by(sortColumn)
         else:
-            productsList = Products.objects.all().order_by(sortColumn)[0:10]
-        return render(req, 'productshow.html',
-                      {'productTypes': ProdTyp.objects.order_by('typeid'),
-                       'productsList': productsList,
-                       'categoryid': str(categoryid),
-                       'childTypes': childTypes,
-                       'chilid': str(chilid),
-                       'sortid': sortid})
-    prodTyps= ProdTyp.objects.all()
-    return render(req,'productshow.html', {'productlist': Products.objects.filter(typeid=id).all()})
+            productslist = Products.objects.filter(categoryid=categoryid).order_by(sortColumn)
+
+    else:
+        productslist = Products.objects.all().order_by(sortColumn)[0:10]
+    return render(req, 'productshow.html',
+                  {'productTypes': ProdTyp.objects.order_by('typeid'),
+                   'categoryid': str(categoryid),
+                   'childTypes': childTypes,
+                   'chilid': str(chilid),
+                   'sortid': sortid,
+                   'productlist':Products.objects.filter(typeid=categoryid).all()})
+
+    # return render(req, 'productshow.html', {'productlist': Products.objects.filter(typeid=categoryid).all()})
